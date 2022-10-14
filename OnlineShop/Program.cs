81b7ef79175107897e25ec060ec7ee072d7d85b0
Christophe BARBIER <christophe.barbier@vilavi.fr>
2022-10-14 15:28:12 +0200

    end clean up

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using OnlineShop.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+// builder.Services.AddRazorPages();
+// builder.Services.AddServerSideBlazor();
+
 
 var app = builder.Build();
 
